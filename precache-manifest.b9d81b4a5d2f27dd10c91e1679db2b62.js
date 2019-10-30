59c803e1ba428c098ae933c06b3255ec4e5ad8ce
Dexer <dex3r@mail.ru>
2019-10-30 13:26:49 +0500

    Add companyList

---
+++ precache-manifest.b9d81b4a5d2f27dd10c91e1679db2b62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a671439cb16aec7f98c5a8f76e3ad4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bf93b006186172011274",
+    "url": "./static/css/main.fba11a11.chunk.css"
+  },
+  {
+    "revision": "eeebe913830751b9fc51",
+    "url": "./static/js/2.ee8b6b7c.chunk.js"
+  },
+  {
+    "revision": "bf93b006186172011274",
+    "url": "./static/js/main.467af788.chunk.js"
+  },
+  {
+    "revision": "f9268e0f2cf269cb6f37",
+    "url": "./static/js/runtime~main.56baa754.js"
+  }
+]);
